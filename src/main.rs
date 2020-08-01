36b6147267f2dc2dc9dda526274b859a80efffd9
Alexander Dikelsky <alexander.dikelsky@gmail.com>
2020-08-01 13:52:38 -0500

    Added circles

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,12 @@ mod shapes;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
-
-
-    let root = BitMapBackend::new("map.png", (1024, 768)).into_drawing_area();
+    let root = BitMapBackend::new("map.png", (1024, 1024)).into_drawing_area();
 
     root.fill(&WHITE)?;
 
     //num_lines changes number of parallels and meridians
-    let num_lines = 8;
+    let num_lines = 4;
     //num_points changes how many points are plotted on each meridian
     let num_points = 1000;
     //Size of the graph. For Mercator projection, should be larger
@@ -24,24 +22,30 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     //Add parallels and meridians
     let mut points: Vec<(f64, f64)> = coord_plane::sphere_coords(num_lines, num_points);
 
+
+
     //***
     //Add shapes here to see distortion
     //***
+    //
+    //**** Square
     //points.extend(shapes::connect_lines(
     //        vec![
     //            (0.0, 0.0),
-    //            (1.0, -1.0),
+    //            (1.0, 0.0),
     //            (1.0, 1.0),
     //            (0.0, 1.0),
     //        ], 500));
+    //**** Circle
+    points.extend(shapes::circle((0.0, 0.0), 1.0, 100));
 
     // *******************************************
     // Change the function called here to remap
     // Running one at a time produces that type of projection
     // *******************************************
     //points = projections::mercator(&points);
-    //points = projections::bonne(&points, FRAC_PI_2);
-    points = projections::simple_equidistant_conic(&points, FRAC_PI_2 * (3.0/4.0), FRAC_PI_2 * (1.0/4.0));
+    points = projections::bonne(&points, FRAC_PI_2);
+    //points = projections::simple_equidistant_conic(&points, FRAC_PI_2 * (3.0/4.0), FRAC_PI_2 * (1.0/4.0));
 
 
     let mut scatter_ctx = ChartBuilder::on(&root)
--- src/shapes.rs
+++ src/shapes.rs
@@ -1,4 +1,5 @@
 use super::coord_plane::points_between_exclusive;
+use core::f64::consts::PI;
 
 //Takes list of pairs, and draws lines from the first pair to the second, and so on
 //The last pair connects back to the first pair
@@ -28,4 +29,12 @@ fn line_exclusive(a: (f64, f64), b: (f64, f64), num_points: usize) -> Vec<(f64, 
         .map(|(i, j)| (*i, *j)).collect()
 }
 
-     
+
+// Takes rectangular point, and returns num_points that are radius r away
+pub fn circle(point: (f64, f64), r: f64, num_points: usize) -> Vec<(f64, f64)> {
+    points_between_exclusive(0.0, 2.0 * PI, num_points).iter()
+        .map(|x|
+            (point.0 + (r * x.cos()),
+             point.1 + (r * x.sin()))
+            ).collect()
+}
