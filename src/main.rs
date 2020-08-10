748334560cf8cc852ed925772c3bb6c1916c112d
Alexander Dikelsky <alexander.dikelsky@gmail.com>
2020-08-10 10:04:35 -0500

    Trying to calculate indicatries
    Not working right now, but the plan is to test tons of points to
    see if they satisfy an equation. Uses probably at least one more
    pass than new scary

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     root.fill(&WHITE)?;
 
     //num_lines changes number of parallels and meridians
-    let num_lines = 8;
+    let num_lines = 4;
     //num_points changes how many points are plotted on each meridian
     let num_points = 1000;
     //Size of the graph. For Mercator projection, should be larger
-    let bound = 2.0;
+    let bound = 4.0;
 
     //Add parallels and meridians
     let mut points: Vec<(f64, f64)> = coord_plane::sphere_coords(num_lines, num_points);
@@ -32,7 +32,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mapping_function: Box<dyn std::ops::Fn(std::vec::Vec<(f64, f64)>) -> std::vec::Vec<(f64, f64)>> = 
         //Box::new(|vals| projections::simple_equidistant_conic(vals.to_vec(), FRAC_PI_2 * (3.0/4.0), FRAC_PI_2 * (1.0/4.0)));
         //Box::new(|vals| projections::bonne(vals, FRAC_PI_2));
-        Box::new(projections::mercator);
+        //Box::new(projections::mercator);
+        Box::new(|x| x);   // Equirectagular
 
     //***
     //Add shapes here to see distortion
@@ -50,9 +51,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     //points.extend(shapes::circle((-1.0, 1.0), 2.0, 100));
 
 
-    //points = mapping_function(points);
+    points = mapping_function(points);
     let indic = tissot_indicatrix::gen_indicatrices(Box::new(mapping_function), num_lines, num_points);
-    points = indic;
+    //points = indic;
 
     let mut scatter_ctx = ChartBuilder::on(&root)
         .x_label_area_size(40)
@@ -68,6 +69,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             .iter()
             .map(|(x, y)| Circle::new((*x, *y), 2, BLACK.filled())),
     )?;
+    scatter_ctx.draw_series(
+        indic.iter()
+            .map(|(x, y)| Circle::new((*x, *y), 2, RED.filled()))
+            )?;
 
     Ok(())
 }
--- src/shapes.rs
+++ src/shapes.rs
@@ -1,5 +1,7 @@
 use super::coord_plane::points_between_exclusive;
 use core::f64::consts::PI;
+use core::f64::consts::FRAC_PI_2;
+const spots_to_check: usize = 100;
 
 pub struct Circle {
     pub center: (f64, f64),
@@ -10,8 +12,9 @@ pub struct Circle {
 impl Circle {
 
 
-    //Vec of points and the center of the circle
-    pub fn to_indicatrix(&self) -> super::tissot_indicatrix::Indicatrix {
+    //This function creates an "indicatrix" that would be correct if distances worked like
+    //they do on a plane. Doesn't actually create an indicatrix.
+    pub fn to_circlular_points(&self) -> super::tissot_indicatrix::Indicatrix {
         super::tissot_indicatrix::Indicatrix {points: points_between_exclusive(0.0, 2.0 * PI, self.num_points).iter()
             .map(|x|
                 (self.center.0 + (self.radius * x.cos()),
@@ -21,7 +24,64 @@ impl Circle {
         }
     }
 
+    pub fn to_indicatrix(&self) -> super::tissot_indicatrix::Indicatrix {
+        super::tissot_indicatrix::Indicatrix {
+            points: {
+                //These points all have the same great circle distance from the center
 
+                // angle from center of sphere =
+                //    acos(sin(φ1)sin(φ2) + cos(φ1)cos(φ2)cos(|λ1-λ2|)
+
+                let sigma = self.radius;
+                let lambda_2 = self.center.0;
+                let phi_2 = self.center.1;
+
+                //Idea is this:
+                //if (rand_point statisfies 
+                //    angle == acos(sin(φ1)sin(φ2) + cos(φ1)cos(φ2)cos(|λ1-λ2|))) {
+                //    add point
+                //}
+
+                let max_dist = 1.0;
+
+                let lat_bound = (lambda_2 - max_dist, lambda_2 + max_dist);
+                let lon_bound = (phi_2 - max_dist, phi_2 + max_dist);
+
+                let guess_points:
+                    Vec<(f64, f64)> = 
+                        points_between_exclusive(lat_bound.0, lat_bound.1, spots_to_check).iter().map(
+                        |x| {
+                            points_between_exclusive(lon_bound.0, lon_bound.1, spots_to_check).iter().map(
+                            |y| (*x, *y) // tuple
+                            ).collect::<Vec<(f64,f64)>>()  // finish first iterator
+                        }
+                    ).flatten().collect();
+
+                guess_points.iter().filter(
+                    |(lambda_1, phi_1)| {
+                    
+                        //dbg!(sigma, 
+                        //        (phi_1.sin() * phi_2.sin() + 
+                        //    phi_1.cos() * phi_2.cos() * (**lambda_1 - lambda_2).abs().cos()).acos());
+
+                        f64_in_tolerance(
+                            sigma, (
+                            phi_1.sin() * phi_2.sin() + 
+                            phi_1.cos() * phi_2.cos() * (*lambda_1 - lambda_2).abs().cos()
+                            ).acos(),
+                            10.0)
+                    })
+                    .map(|(x,y)| (*x,*y)).collect()
+            },
+            center: self.center,
+        }
+
+    }
+
+}
+
+fn f64_in_tolerance(num1: f64, num2: f64, tolerance: f64) -> bool {
+    (num1-num2).abs() < tolerance
 }
 
 
